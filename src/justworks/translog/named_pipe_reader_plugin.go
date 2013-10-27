5adab1e6ffc633371bcfc1be29e554ffc78a5e06
Rene Lengwinat <rene.lengwinat@thoughtworks.com>
2013-10-27 16:06:50 +0100

    [NamedPipeReader] made reader implementation more bullet proof, still needs a lot of cleanup

--- src/justworks/translog/named_pipe_reader_plugin.go
+++ src/justworks/translog/named_pipe_reader_plugin.go
@@ -16,7 +16,8 @@ type NamedPipeReaderPlugin struct {
   config          map[string]string
   debug           bool
   bufsize         int
-  poll_interval   int
+  poll_interval   time.Duration
+  check_interval  int
   _last_error_msg string
   _last_error_cnt int
 }
@@ -45,7 +46,8 @@ func (plugin *NamedPipeReaderPlugin) Configure(config map[string]string) {
     plugin.bufsize = int(val)
   }
 
-  plugin.poll_interval = 1
+  plugin.poll_interval = time.Duration(1) * time.Second
+  plugin.check_interval = 10
 }
 
 // TODO: extract into common function
@@ -73,49 +75,113 @@ func (plugin *NamedPipeReaderPlugin) logError2(stage string, err string) {
   }
 }
 
-func (plugin *NamedPipeReaderPlugin) Start(c chan *Event) {
-  config := plugin.config
-  source := fmt.Sprintf("pipe://%s", config["source"])
-
-  pipe, err := os.OpenFile(config["source"], os.O_RDONLY, 0600)
+func (plugin *NamedPipeReaderPlugin) openPipe() (*os.File, os.FileInfo, error) {
+  pipe, err := os.OpenFile(plugin.config["source"], os.O_RDONLY, 0600)
 
   if err != nil {
     plugin.logError2("openFile", err.Error())
-    return
+    return pipe, nil, err
+  }
+
+  pipeStat, err := pipe.Stat()
+
+  if err != nil {
+    plugin.logError2("pipeStat", err.Error())
+    return nil, nil, err
   }
 
   if err = syscall.SetNonblock(int(pipe.Fd()), true); err != nil {
     log.Printf("[%T] Failed opening file in NONBLOCKING mode (err='%s')", err.Error())
   }
 
+  return pipe, pipeStat, nil
+}
+
+func (plugin *NamedPipeReaderPlugin) Start(c chan *Event) {
+  config := plugin.config
+  source := fmt.Sprintf("pipe://%s", config["source"])
+
+  pipe, pipeStat, err := plugin.openPipe()
+
+  if err != nil {
+    log.Fatalf("[%T] failed to open named pipe (err='%s')", err.Error())
+  }
+
   buf := bytes.NewBufferString("")
 
+  check_loop_cnt := 0
+  do_read := true
+
   for {
-    rbuf := make([]byte, plugin.bufsize)
-    n, err := pipe.Read(rbuf)
+    if check_loop_cnt >= plugin.check_interval {
+      if plugin.debug {
+        log.Printf("[%T] checking for changes on named pipe %s", plugin, config["source"])
+      }
 
-    if n != 0 && err == nil {
-      for _, char := range rbuf {
-        if char != '\n' {
-          buf.WriteByte(char)
+      fi, err := os.Stat(config["source"])
+
+      if err != nil {
+        if do_read {
+          plugin.logError2("checkPipe1", err.Error())
+        }
+        do_read = false
+      }
+
+      if !os.SameFile(pipeStat, fi) {
+        if plugin.debug {
+          log.Printf("[%T] detected change in named pipe, reloading %s", plugin, config["source"])
+        }
+
+        pipe, pipeStat, err = plugin.openPipe()
+
+        if err != nil {
+          if do_read {
+            plugin.logError2("openNewPipe", err.Error())
+          }
+
+          do_read = false
         } else {
-          if len(strings.TrimSpace(buf.String())) > 0 {
-            event := CreateEvent(source)
-            event.SetRawMessage(buf.String())
+          log.Printf("[%T] detected change in named pipe, reloaded %s", plugin, config["source"])
+          do_read = true
+        }
+      } else {
+        do_read = true
+      }
 
-            c <- event
-            buf.Reset()
+      check_loop_cnt = 0
+    }
+
+    check_loop_cnt += 1
+
+    if do_read {
+      rbuf := make([]byte, plugin.bufsize)
+      n, err := pipe.Read(rbuf)
+
+      if n != 0 && err == nil {
+        for _, char := range rbuf {
+          if char != '\n' {
+            buf.WriteByte(char)
+          } else {
+            if len(strings.TrimSpace(buf.String())) > 0 {
+              event := CreateEvent(source)
+              event.SetRawMessage(buf.String())
+
+              c <- event
+              buf.Reset()
+            }
           }
         }
       }
-    }
 
-    if n == 0 && err != nil && err != io.EOF {
-      plugin.logError2("write", err.Error())
-    }
+      if n == 0 && err != nil && err != io.EOF {
+        plugin.logError2("write", err.Error())
+      }
 
-    if n == 0 || err == io.EOF {
-      time.Sleep(time.Duration(plugin.poll_interval) * time.Second)
+      if n == 0 || err == io.EOF {
+        time.Sleep(plugin.poll_interval)
+      }
+    } else {
+      time.Sleep(plugin.poll_interval)
     }
   }
 }
